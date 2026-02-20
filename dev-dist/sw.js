b5be75f2c5503b8ab5711a7e415c579a20796918
mateo <mateo1559@gmail.com>
2026-02-20 22:12:36 +0100

    feat: update deployment workflow and GTFS data processing steps

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d975e299'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.l47t6368ce8"
+    "revision": "0.1s6f9r5aonk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
